9de1343622336c8b58705440dade623b4600033e
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-12-09 09:07:29 -0700

    #13

--- src/js/1p-filters.js
+++ src/js/1p-filters.js
@@ -324,6 +324,13 @@ var editor = ace.edit('userFilters');
 editor.getSession().setMode('ace/mode/nano_filters');
 editor.$blockScrolling = Infinity;
 
+// Patch 2017-12-09: Fix line ending, Ace's auto-detect feature is broken
+if ( navigator.userAgent.includes('Windows') ) {
+    editor.getSession().setNewLineMode('windows');
+} else {
+    editor.getSession().setNewLineMode('unix');
+}
+
 /******************************************************************************/
 
 var messaging = vAPI.messaging;
