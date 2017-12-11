c4267e09fe1ec2d74c6b8c2ecf6774bac2f54186
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-12-11 10:34:56 -0700

    extract ide config logic

---
+++ src/js/nano-ide.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/******************************************************************************/
+
+self.nanoIDE = self.nanoIDE || {};
+
+/******************************************************************************/
+
+self.nanoIDE.init = function(editor, highlight, readonly) {
+    var session = editor.getSession();
+
+    if ( highlight ) {
+        session.setMode('ace/mode/nano_filters');
+    } else {
+        session.setMode('ace/mode/text');
+    }
+
+    editor.setReadOnly(readonly);
+
+    // Patch 2017-12-10: Disable commands that makes no sense
+    var uselessCommands = ['blockindent', 'blockoutdent', 'indent', 'outdent'];
+    for ( var cmd of uselessCommands ) {
+        editor.commands.removeCommand(cmd);
+    }
+
+    // Patch 2017-12-09: Fix line ending, Ace's auto-detect feature is broken
+    if ( navigator.userAgent.includes('Windows') ) {
+        session.setNewLineMode('windows');
+    } else {
+        session.setNewLineMode('unix');
+    }
+
+    editor.$blockScrolling = Infinity;
+};
