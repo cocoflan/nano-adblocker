0fa94fc990fa077b9bf8f74d3a00b88c42a9bb89
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-12-17 10:56:03 -0700

    draft 3

--- src/js/nano-dashboard.js
+++ src/js/nano-dashboard.js
@@ -4,3 +4,277 @@
 
 "use strict";
 
+// ===== Main Controllers =====
+
+/**
+ * The tab that is currently active.
+ * @var {Tab}
+ */
+let currentTab = null;
+
+/**
+ * Update action buttons for this tab.
+ * The two arrays that are passed in must be parallel arrays.
+ * @method
+ * @param {Array.<string>} inIcons - The icons to set.
+ * @param {Array.<string>} inTooltips - The tooltips to set.
+ * @param {Array.<Function>} inHandlers - The click handlers.
+ * @return {Array.<HTMLElement>} The updated buttons.
+ */
+const drawActionBtns = (() => {
+    const buttons = [...document.querySelectorAll("[id^='nano-action-']")];
+    const icons = document.querySelectorAll("[id^='nano-action-'] > span");
+    const tooltips = document.querySelectorAll("[data-mdl-for^='nano-action-']");
+
+    return (inIcons, inTooltips, inHandlers) => {
+        for (let i = 0; i < buttons.length; i++) {
+            if (i < inIcons.length) {
+                icons[i].textContent = inIcons[i];
+                tooltips[i].textContent = inTooltips[i];
+                buttons[i].onclick = inHandlers[i];
+                buttons[i].style.display = "block";
+            } else {
+                buttons[i].onclick = null;
+                buttons[i].style.display = "none";
+            }
+        }
+        return buttons.slice(0, inIcons.length);
+    };
+})();
+
+/**
+ * Tab interface.
+ * @class
+ */
+const Tab = class {
+    /**
+     * Constructor.
+     * @constructor
+     * @param {HTMLElement} btn - The tab bar button.
+     * @param {HTMLElement} content - The content section.
+     */
+    constructor(btn, content) {
+        this.btn = btn;
+        this.content = content;
+
+        this.btn.addEventListener("click", () => {
+            if (currentTab) {
+                if (currentTab === this) {
+                    return;
+                }
+                if (!currentTab.teardown(this)) {
+                    return;
+                }
+            }
+            this.init();
+        });
+    }
+    /**
+     * Initialize the tab.
+     * @method
+     */
+    init() {
+        this.btn.classList.add("is-active");
+        this.content.removeAttribute("hidden");
+        currentTab = this;
+    }
+    /**
+     * Teardown current tab. If overriden, should take in the caller tab in case teardown
+     * succeeded asynchronously.
+     * @method
+     * @return {boolean} True if the operation succeeded, false otherwise.
+     */
+    teardown() {
+        this.btn.classList.remove("is-active");
+        this.content.setAttribute("hidden", "");
+        return true;
+    }
+};
+
+// ===== Tab Objects =====
+
+/**
+ * The config tab.
+ * @const {Tab}
+ */
+const tabConfig = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-config"),
+            document.getElementById("nano-section-config"),
+        );
+    }
+    /**
+     * No action buttons for config tab.
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns([], [], []);
+    }
+};
+
+/**
+ * The filters tab.
+ */
+const tabFilters = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-filters"),
+            document.getElementById("nano-section-filters"),
+        );
+    }
+    /**
+     * 3 buttons: Update now, Purge all caches, Apply changes
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns(
+            ["update", "delete_forever", "done"],
+            [vAPI.i18n("3pUpdateNow"), vAPI.i18n("3pPurgeAll"), vAPI.i18n("3pApplyChanges")],
+            // TODO
+            [() => { }, () => { }, () => { }],
+        );
+    }
+    /**
+     * Check for unsaved changes.
+     */
+    teardown() {
+        // TODO 2017-12-17: Check for unsaved changes 
+        super.teardown();
+        return true;
+    }
+};
+
+/**
+ * The rules tab.
+ */
+const tabRules = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-rules"),
+            document.getElementById("nano-section-rules"),
+        );
+    }
+    /**
+     * 4 buttons: Apply changes, Revert, Import and append, Export
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns(
+            ["done", "undo", "note_add", "file_download"],
+            [vAPI.i18n("1pApplyChanges"), vAPI.i18n("genericRevert"), vAPI.i18n("1pImport"), vAPI.i18n("1pExport")],
+            // TODO
+            [() => { }, () => { }, () => { }, () => { }],
+        );
+    }
+    /**
+     * Check for unsaved changes.
+     */
+    teardown() {
+        // TODO 2017-12-17: Check for unsaved changes 
+        super.teardown();
+        return true;
+    }
+};
+
+/**
+ * The whitelist tab.
+ */
+const tabWhitelist = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-whitelist"),
+            document.getElementById("nano-section-whitelist"),
+        );
+    }
+    /**
+     * 4 buttons: Apply changes, Revert, Import and append, Export
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns(
+            ["done", "undo", "note_add", "file_download"],
+            [vAPI.i18n("whitelistApply"), vAPI.i18n("genericRevert"), vAPI.i18n("whitelistImport"), vAPI.i18n("whitelistExport")],
+            // TODO
+            [() => { }, () => { }, () => { }, () => { }],
+        );
+    }
+    /**
+     * Check for unsaved changes.
+     */
+    teardown() {
+        // TODO 2017-12-17: Check for unsaved changes 
+        super.teardown();
+        return true;
+    }
+};
+
+/**
+ * The matrix tab.
+ */
+const tabMatrix = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-matrix"),
+            document.getElementById("nano-section-matrix"),
+        );
+    }
+    /**
+     * No action buttons for matrix tab.
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns([], [], []);
+    }
+    /**
+     * Check for unsaved changes.
+     */
+    teardown() {
+        // TODO 2017-12-17: Check for unsaved changes 
+        super.teardown();
+        return true;
+    }
+};
+
+/**
+ * The about tab.
+ * @const {Tab}
+ */
+const tabAbout = new class extends Tab {
+    /**
+     * Pass appropriate elements to super.
+     */
+    constructor() {
+        super(
+            document.getElementById("nano-tab-about"),
+            document.getElementById("nano-section-about"),
+        );
+    }
+    /**
+     * No action buttons for about tab.
+     */
+    init() {
+        super.init();
+        this.buttons = drawActionBtns([], [], []);
+    }
+};
+
+// ===== Bootstrap =====
+
+// TODO: read localstorage for last active tab etc.
+tabConfig.init();
