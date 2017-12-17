7437f3bbc822652a2c9819d63e1161aad08d4fa1
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-12-16 21:58:04 -0700

    initial framework

---
+++ src/js/nano-dashboard.js
@@ -0,0 +1,6 @@
+// Patch 2017-12-16: Add an alternative dashboard.
+// This file should be formatted by Visual Studio and use my own code style.
+// This new dashboard will only work for modern browsers.
+
+"use strict";
+
